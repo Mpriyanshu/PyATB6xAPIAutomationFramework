0529a56b928c892d2de26f635b4d328471ad6bd6
new <tiwaripriyanshu640@gmail.com>
2025-12-28 16:36:45 +0530

    18 dec code pushed/first

---
+++ src/tests/crud/test_create_booking.py
@@ -0,0 +1,47 @@
+import allure
+import pytest
+
+from src.modules.wrapper.api_requests_wrapper import post_request
+from src.endpoints.api_constansts import APIConstants
+from src.utils.utils import Utils
+from src.modules.payload_manager.payload_manager import payload_create_booking
+from src.modules.verfication.common_verification import *
+
+class TestCreateBooking:
+    @pytest.mark.positive
+    @allure.title("Verify that Create Booking Status and Booking ID shouldn't be null")
+    @allure.description("Creating a Booking from the payload and Verify that Booking ID should not be null"
+                        "and status code should be 200 for the correct payload")
+    def test_create_booking_positive(self):
+        response = post_request(
+            url=APIConstants().url_create_booking(),
+            auth = None,
+            headers = Utils().common_headers_json(),
+            payload = payload_create_booking(),
+            in_json = False
+        )
+
+        verify_http_status_code(response_data_status=response.status_code, expected_data=200)
+        verify_json_key_for_not_null(response.json()["bookingid"])
+
+    @allure.title("#2 test_booking_create_negative_tc1")
+    def test_booking_create_negative_tc1(self):
+        response = post_request(
+            url=APIConstants().url_create_booking(),
+            auth=None,
+            headers=Utils().common_headers_json(),
+            payload={"firstname": "Aman"},
+            in_json=False
+        )
+        verify_http_status_code(response_data_status=response.status_code, expected_data=500)
+
+    @allure.title("#3 test_booking_create_negative_tc2")
+    def test_booking_create_negative_tc2(self):
+        response = post_request(
+            url=APIConstants().url_create_booking(),
+            auth=None,
+            headers=Utils().common_headers_json(),
+            payload={"firstname": "Promo"},
+            in_json=False
+        )
+        verify_http_status_code(response_data_status=response.status_code, expected_data=500)
--- src/tests/sample/test_sample.py
+++ src/tests/sample/test_sample.py
@@ -1,11 +1,13 @@
 import pytest
 import allure
 
+@allure.step("Step 1")
 @allure.title("Verify ❌ Failed ! that the Framework is working")
 def test_sample_pass():
     assert True == False
 
 
+@allure.step("Step 2")
 @allure.title("Verify ✅ Passed that the Framework is working")
 def test_sample_fail():
     assert True == True
